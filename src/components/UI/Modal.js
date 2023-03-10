78453de27361144f635314ba6c0b588963bb9442
Rene Mercadel <renemercadel@gmail.com>
2023-03-10 15:58:02 -0600

    Implemented Lecture Version of the Cart Modal Open & Close Functionality Successfully.

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}>Backdrop</div>;
+  return (
+    <div className={classes.backdrop} onClick={props.onClose}>
+      Backdrop
+    </div>
+  );
 };
 
 const ModalOverlay = (props) => {
@@ -20,7 +24,10 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
