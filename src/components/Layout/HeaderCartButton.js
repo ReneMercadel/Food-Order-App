d315716c2ebd06896591f56f2e7453c68be74099
Rene Mercadel <renemercadel@gmail.com>
2023-03-10 16:34:02 -0600

    Implemented Initial Cart Context, Provider, and Reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartContext = useContext(CartContext);
+  console.log(
+    'cartContext: ',
+    cartContext.items.reduce((totalItems, item) => {
+      return totalItems + item.amount;
+    }, 0)
+  );
+
+  const numberOfCartItems = cartContext.items.length;
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
